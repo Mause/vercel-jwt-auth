92b79c24737a8511d3a3a50ca0a1da3bfbcc2327
Elliana May <me@mause.me>
2021-08-26 11:49:07 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -36,14 +36,3 @@ export function factory(secret: string) {
     };
   };
 }
-
-declare global {
-  module "@vercel/node" {
-    // tslint:disable-next-line:no-empty-interface
-    interface User {}
-
-    interface VercelRequest {
-      user?: User | undefined;
-    }
-  }
-}
