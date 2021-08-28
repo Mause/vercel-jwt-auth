1817819a28c977dfb8202c787b39d9c9ccb103b4
Elliana May <me@mause.me>
2021-08-28 21:55:43 +0800

    Remove global

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,10 @@
-declare global {
-  declare module "@vercel/node" {
-    interface User {
-      email: string;
-      id: string;
-    }
+declare module "@vercel/node" {
+  interface User {
+    email: string;
+    id: string;
+  }
 
-    interface VercelRequest {
-      user?: User | undefined;
-    }
+  interface VercelRequest {
+    user?: User | undefined;
   }
 }
