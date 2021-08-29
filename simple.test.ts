df6571d9556be4ef2534dad8dd49c5010642f14a
Elliana <me@mause.me>
2021-08-29 16:01:51 +0800

    Add tests

---
+++ simple.test.ts
@@ -0,0 +1,52 @@
+import { factory } from "./index";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import JWT from "jsonwebtoken";
+
+const SECRET = "...";
+
+const endpoint = factory(SECRET)((req, res) => {
+  res.status(200);
+  res.json("OK");
+});
+
+let response: VercelResponse;
+let resolve: (value: unknown) => void;
+let jsonPromise: Promise<unknown>;
+beforeEach(() => {
+  jsonPromise = new Promise((_resolve) => (resolve = _resolve));
+  response = {
+    status: jest.fn(),
+    json(content: unknown) {
+      resolve(content);
+    },
+  } as unknown as VercelResponse;
+});
+
+test("Basic", async () => {
+  endpoint({ headers: {} } as VercelRequest, response);
+
+  expect(await jsonPromise).toEqual("No authorization token was found");
+});
+test("Bad secret", async () => {
+  withHeader(JWT.sign({ hello: "world", aud: "authenticated" }, "BAD SECRET"));
+
+  expect(await jsonPromise).toEqual("invalid signature");
+});
+test("Authed", async () => {
+  jest.setTimeout(2000000);
+
+  withHeader(JWT.sign({ hello: "world", aud: "authenticated" }, SECRET));
+
+  expect(await jsonPromise).toEqual("OK");
+});
+
+function withHeader(header: string) {
+  endpoint(
+    {
+      headers: {
+        authorization: "Bearer " + header,
+      },
+    } as unknown as VercelRequest,
+    response
+  );
+}
